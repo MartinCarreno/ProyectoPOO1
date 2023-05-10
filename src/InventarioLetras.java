502fb050ff8f9535223076103b1652ff91d209b0
MartinCarreno <martincarreno71@gmail.com>
2023-05-09 22:59:04 -0400

    Definicion de metodos por defecto

--- src/InventarioLetras.java
+++ src/InventarioLetras.java
@@ -2,8 +2,53 @@
 public class InventarioLetras {
 	private String data;
 	
+	
 	public InventarioLetras(String data) {
 		this.data = data;
 	}
 	
+	public char encriptarCesar(char letra) {
+		
+		return 'a';
+	}
+	
+	public void encriptarPalabra ( char letra , int valor ) {
+		
+	}
+	
+	public void desencriptarPalabra ( char letra , int valor ) {
+		
+	}
+	
+	public int get ( char letra ) {
+		int contLetra = 0;
+		
+		return contLetra;
+	}
+	
+	public void set ( char letra , int valor ) {
+		
+	}
+	
+	public int size () {
+		
+	}
+	
+	public boolean isEmpty () {
+		
+	}
+	
+	public String toString () {
+		
+	}
+	
+	public InventarioLetras add ( InventarioLetras otro ) {
+		
+	}
+	public InventarioLetras amplifies (int n ) {
+		
+	}
+	public InventarioLetras subtract ( InventarioLetras otro ) {
+		
+	}
 }
