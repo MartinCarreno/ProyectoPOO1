89c137af14bec0aeed75167b273cab910a6e5642
marti <marti@Martin>
2023-05-29 12:46:10 -0400

    Cambio de 'ñ' a unicode \u00f1 para que no haya errores

--- src/LanguageEs.java
+++ src/LanguageEs.java
@@ -1,7 +1,7 @@
 
 public class LanguageEs extends Language{
 	
-	private String abecedarioEs = super.getAbecedario().substring(0,14) + 'ñ' + super.getAbecedario().substring(14);
+	private String abecedarioEs = super.getAbecedario().substring(0,14) + '\u00f1' + super.getAbecedario().substring(14);
 	
 	
 	
